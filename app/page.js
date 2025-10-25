12726f1a6f99db623f7572192c7d538742022fbe
Sb2304 <shivrajbishnoi9000@gmail.com>
2025-10-25 10:46:45 +0530

    developed main page

--- app/page.js
+++ app/page.js
@@ -1,23 +1,24 @@
-"use client"
 import Image from "next/image";
 
 export default function Home() {
-  const ClickHandle = () => {
-    console.log("Button Clicked");
-  };
   return (
     <>
-      <div className="flex flex-col justify-center items-center h-[44vh] text-white">
-        <div className="font-bold text-5xl flex justify-center items-center gap-2">TipJar <span><img className="w-20 p-0" src="/money-jar.svg" alt="jar" /></span></div>
+      <div className="flex flex-col justify-center items-center h-[44vh] text-white gap-5">
+        <div className="font-bold text-5xl flex justify-center items-center gap-2">
+          TipJar
+          <span>
+            <img className="size-20 p-0" src="/money-jar-2.svg" alt="jar" />
+          </span>
+        </div>
         <p>
           A simple way to tip your favourite creators, developers, and artists.
         </p>
         <div>
           <button
             type="button"
-            className="text-white bg-gradient-to-br hover:cursor-pointer from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" onClick={ClickHandle}
+            className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
           >
-            Start Now
+            Start Now!
           </button>
           <button
             type="button"
@@ -27,8 +28,44 @@ export default function Home() {
           </button>
         </div>
       </div>
-      <div className="bg-white h-1 opacity-10">
-        eadadawd
+      <div className="bg-white h-1 opacity-10"></div>
+
+      <div className="text-white container mx-auto py-20">
+        <h2 className="text-2xl font-bold text-center mb-10">
+          Your supporters can appreciate you!
+        </h2>
+        <div className="flex gap-5 justify-around">
+          <div className="item flex flex-col items-center justify-center space-y-3">
+            <img
+              src="designer.svg" className="size-40 p-1 bg-amber-800/50 rounded-full" alt=""
+            />
+            <p className="font-bold">Fund yourself</p>
+            <p className="text-center">Turn community support into your next creative project.</p>
+          </div>
+          <div className="item flex flex-col items-center justify-center space-y-3">
+            <img
+              src="passion.svg" className="size-40 p-1 bg-amber-800/50 rounded-full" alt=""
+            />
+            <p className="font-bold">Fuel Your Passion</p>
+            <p className="text-center">Turn community support into your next creative project.</p>
+          </div>
+          <div className="item flex flex-col items-center justify-center space-y-3">
+            <img
+              src="community.svg" className="size-40 p-1 bg-amber-800/50 rounded-full" alt=""
+            />
+            <p className="font-bold">Connect with Supporters</p>
+            <p className="text-center">Provide a path for your audience to show direct appreciation.</p>
+          </div>
+        </div>    
+      </div>
+
+      <div className="bg-white h-1 opacity-10"></div>
+
+      <div className="text-white container mx-auto py-20 flex flex-col items-center justify-center">
+        <h2 className="text-2xl font-bold text-center mb-10">
+          Learn more about us
+        </h2>
+        <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/ym1zJGAW3WE?si=cFZj33EMbRxLyijf" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen className="rounded-md drop-shadow-amber-500 drop-shadow-sm focus:bg-transparent"></iframe>
       </div>
     </>
   );
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,16 +1,23 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
     <nav className='bg-stone-800 text-white p-4 flex justify-between items-center'>
-      <div className="logo font-bold text-4xl">TipJar</div>
-      <ul className='flex justify-between gap-3'>
+      <Link href={"/"}><div className="logo font-bold text-4xl flex justify-center items-center">TipJar <span><img className='size-12' src="/money-jar-2.svg" alt="" /></span></div></Link>
+      {/* <ul className='flex justify-between gap-3'>
         <li>Home</li>
         <li>About</li>
         <li>Projects</li>
         <li>Sign Up</li>
         <li>Login</li>
-      </ul>
+      </ul> */}
+
+      <div>
+        <Link href={"/login"}>
+          <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none   focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Login</button>
+        </Link>
+      </div>
     </nav>
   )
 }
